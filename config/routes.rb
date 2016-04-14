cc1f0624177cdfb2523167e7ddbc28ef196a7292
Sam O'Keefe <samok13@gmail.com>
2016-04-14 16:47:54 -0700

    registrations, sessions testing

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   scope :api do
     scope :v1 do
 
-      devise_for :users, controllers: { sessions: 'users/sessions' } do
+      devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' } do
         get "sign_in" => "users/sessions#new"
         delete "sign_out" => "users/sessions#destroy"
       end
