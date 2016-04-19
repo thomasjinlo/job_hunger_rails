13a6bdefc799ebe809e5ee34ce6b25e70e0e4917
Kit Langton <kitlangton@gmail.com>
2016-04-19 18:54:31 -0400

    alter seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-puts "Deleting everything..."
+puts 'Deleting everything...'
 
 Job.destroy_all
 Lead.destroy_all
@@ -7,21 +7,20 @@ User.destroy_all
 
 MULTIPLIER = 10
 
-puts "Creating users..."
+puts 'Creating users...'
 
 def create_user
   email = Faker::Internet.email
-  User.create(email: email, uid: email, password: "password")
+  User.create(email: email, password: 'password')
 end
 
 MULTIPLIER.times do
   create_user
 end
 
-User.create(email: 'test@test.com', password: "password")
+User.create(email: 'test@test.com', password: 'password')
 
-
-puts "Creating companies..."
+puts 'Creating companies...'
 
 def create_company(user_id)
   Company.create(name: Faker::Company.name, user_id: user_id, notes: Faker::Company.catch_phrase, interest: rand(1..5), website: Faker::Internet.url)
@@ -33,21 +32,15 @@ User.all.each do |user|
   end
 end
 
-
-
-
 def create_lead(company_id)
   Lead.create(company_id: company_id, name: Faker::Name.name, blog: Faker::Internet.url, linked_in: Faker::Internet.url, notes: Faker::Company.catch_phrase)
 end
-
-
 
 def create_job(company_id)
   Job.create(title: Faker::Name.title, application_status: ['Interested', 'Applied', 'Rejected', 'Interview', 'Offer'].sample, url: Faker::Internet.url, company_id: company_id)
 end
 
-
-puts "Creating leads and jobs..."
+puts 'Creating leads and jobs...'
 
 Company.all.each do |company|
   2.times do
@@ -56,6 +49,4 @@ Company.all.each do |company|
   create_job(company.id)
 end
 
-
-
-puts "Done!"
+puts 'Done!'
