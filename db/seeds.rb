849c39fa1b34e57b34a38db785faf366f7e42233
Julia Herron <julia.herron@gmail.com>
2016-04-20 16:18:48 -0700

    fix the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,7 +28,7 @@ def create_company(user_id)
 end
 
 User.all.each do |user|
-  5.times do
+  2.times do
     create_company(user.id)
   end
 end
@@ -41,12 +41,14 @@ def create_job(company_id)
   Job.create(title: Faker::Name.title, application_status: ['Interested', 'Applied', 'Rejected', 'Interview', 'Offer'].sample, url: Faker::Internet.url, company_id: company_id)
 end
 
-puts 'Creating leads and jobs...'
 
-def create_company_recommendations(parent)
-  Company::RECOMMENDATIONS.each do |action|
-    Recommendation.create(recommendable_type: parent.class, recommendable_id: parent.id, completed: false, action: action, user_id: parent.user.id, start_date: DateTime.now + rand(1..3))
-  end
+def create_company_recommendations(company)
+  recommendations = [
+    "Do some google-ing to find #{company.name}'s tech blog", 
+    "Gather some cool facts about #{company.name}"
+  ]
+  action = recommendations.sample
+  Recommendation.create(recommendable_type: company.class, recommendable_id: company.id, completed: false, action: action, user_id: company.user.id, start_date: DateTime.now + rand(1..3))
 end
 
 puts 'Creating leads and jobs...'
@@ -61,10 +63,11 @@ end
 
 puts 'Creating recommendations...'
 
-def create_lead_recommendations(parent)
-  Lead::RECOMMENDATIONS.each do |action|
-    Recommendation.create(recommendable_type: parent.class, recommendable_id: parent.id, completed: false, action: action, user_id: parent.company.user.id, start_date: DateTime.now + rand(1..3))
-  end
+def create_lead_recommendations(lead)
+  recommendations = ["Step up your stalker game and find #{lead.name}'s blog", "Connect with #{lead.name} on LinkedIn", "Is #{lead.name}'s company hosting a Meetup soon?"]
+  action = recommendations.sample
+
+  Recommendation.create(recommendable_type: lead.class, recommendable_id: lead.id, completed: false, action: action, user_id: lead.company.user.id, start_date: DateTime.now + rand(1..3))
 end
 
 Lead.all.each do |lead|
