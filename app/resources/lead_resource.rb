d73a4d34136328d7e906cbca5fd657d384940d85
Julia Herron <julia.herron@gmail.com>
2016-04-20 08:06:28 -0700

    polymorphic, company id required

--- app/resources/lead_resource.rb
+++ app/resources/lead_resource.rb
@@ -3,6 +3,8 @@ class LeadResource < JSONAPI::Resource
 
   attributes :name, :linked_in, :blog, :email, :notes
 
+  after_create :generate_recommendations
+
   def generate_recommendations
     Lead::RECOMMENDATIONS.each do |action|
       Recommendation.create(recommendable_type: @model.class, recommendable_id: @model.id, completed: false, action: action, user_id: @model.company.user.id, start_date: DateTime.now + rand(1..3))
