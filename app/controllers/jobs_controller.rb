8025d40b846c5a6483c4a7a4a611d53a90e51d07
Julia Herron <julia.herron@gmail.com>
2016-04-21 16:01:00 -0700

    create_from_chrome

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,3 +1,18 @@
 class JobsController < ApplicationController
   before_action :authenticate_user!
+
+  def create_from_chrome
+    company = Company.find_or_create_by(name: params[:company_name], user_id: current_user.id)
+    @job = Job.create(title: params[:title], company_id: company.id)
+
+    respond_to do |f|
+      if @job.save
+        f.json { render json: 201 }
+      else
+        f.json { render status: :unprocessable_entity}
+      end
+    end
+  end
+
+
 end
